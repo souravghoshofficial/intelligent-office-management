cf335c80c90cb6a8b2ad3f073de90fbf45adbd3b
souravghoshofficial <souravghosh.tech@gmail.com>
2025-11-05 12:47:28 +0530

    add feature: createEmployee, createDept

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,16 @@ app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 })
 
+
 app.get("/" , (req, res) => {
     res.send("Hello from Intelligent Office Management backend");
 })
+
+
+import adminRouter from "./routes/adminRoute.js"
+import authRoute from "./routes/auth.js"
+
+app.use("/api/admin", adminRouter);
+app.use("/api/auth", authRoute);
+
+
