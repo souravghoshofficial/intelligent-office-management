e0d719f1d148ed9018f682ac1aef62a6e8df4a9e
Abhijit-Rabidas <abhijit.rabidas.mca@gmail.com>
2025-11-13 18:10:01 +0530

    fix hosting problem1

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,34 +1,51 @@
+// import express from "express";
+// import cors from "cors"
+// import dotenv from "dotenv"
+// import cookieParser from "cookie-parser";
+
+// dotenv.config()
+
+// const app = express()
+
+// app.use(cors({
+//     origin: [
+//         process.env.CORS_ORIGIN
+//     ],
+//     credentials: true
+// }))
+
+// app.use(express.json({
+//     limit: "16kb"
+// }))
+
+// app.use(express.urlencoded({
+//     extended: true,
+//     limit: "16kb"
+// }))
+
+// app.use(express.static("public"))
+
+// app.use(cookieParser());
+
+// export {app};
+
 import express from "express";
-import cors from "cors"
-import dotenv from "dotenv"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
-dotenv.config()
+const app = express();
 
-const app = express()
+// CORS
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(cors({
-    origin: [
-        process.env.CORS_ORIGIN
-    ],
-    credentials: true
-}))
-
-
-app.use(express.json({
-    limit: "16kb"
-}))
-
-app.use(express.urlencoded({
-    extended: true,
-    limit: "16kb"
-}))
-
-app.use(express.static("public"))
-
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 app.use(cookieParser());
 
-
-
-
-export {app};
+export { app };
